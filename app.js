ca7e76cd0225f2971397c24a7de493d0ea7e8e6c
root <root@ip-172-31-37-23.ap-south-1.compute.internal>
2022-06-14 10:05:32 +0000

    changes made in app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 // Using send function we send
 // response to the client
 // Here we are sending html
-res.send("<h1> Hello World </h1>");
+res.send("<h1> Hello TGH Tech World </h1>");
 });
 
 // It configures the system to listen
