6f31e42f906da4dba951b54c2153abf1a511d0f0
sreekanthpydi <92301286+sreekanthpydi@users.noreply.github.com>
2022-06-14 15:58:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,18 +13,17 @@ app.get("/", (req, res) => {
 // Using send function we send
 // response to the client
 // Here we are sending html
-res.send("<h1> Hello TGH Tech World </h1>");
+res.send("<h1> Hello World </h1>");
 });
 
 // It configures the system to listen
-// to port 3000. Any number can be 
+// to port 3000. Any number can be
 // given instead of 3000, the only
 // condition is that no other server
-// should be running at the port
+// should be running at that port
 app.listen(3000, () => {
 
 // Print in the console when the
 // servers starts to listen on 3000
 console.log("Listening to port 3000");
 });
-
